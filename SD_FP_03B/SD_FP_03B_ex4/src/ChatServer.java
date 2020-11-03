44eddc07585f1955086bea85cd7ec1ed249b092c
JessicaCoelho21 <jscoelho21@gmail.com>
2020-11-03 10:02:07 +0000

    Worksheet 3B, beginning of exercise 4

---
+++ SD_FP_03B/SD_FP_03B_ex4/src/ChatServer.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.net.*;
+import java.util.LinkedList;
+
+public class ChatServer {
+    private static LinkedList<WorkerThread> chat = new LinkedList<>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = null;
+        int port=2048;
+        boolean listening = true;
+
+        try {
+            serverSocket = new ServerSocket(port);
+        } catch (IOException e) {
+            System.err.println("Could not listen on port: "+port+".");
+            System.exit(-1);
+        }
+
+        while (listening) {
+            WorkerThread wt = new WorkerThread(serverSocket.accept());
+            wt.start();
+            chat.add(wt);
+        }
+        serverSocket.close();
+    }
+
+    public static void sendBroadcast(String text, WorkerThread excludeChat) {
+        for (WorkerThread aChat : chat) {
+            if (aChat != excludeChat) {
+                aChat.sendMessage(text);
+            }
+        }
+    }
+}
