1ce43eab6816c7cfc1e514df16ab2082a989b6f6
JessicaCoelho21 <jscoelho21@gmail.com>
2020-11-10 18:16:12 +0000

    Worksheet 4 completed

---
+++ SD_FP_04/SD_FP_04_ex2/src/ClientWriterThread.java
@@ -0,0 +1,41 @@
+import java.net.*;
+import java.io.*;
+
+public class ClientWriterThread extends Thread {
+    private Socket socket;
+    PrintWriter out = null;
+
+    public ClientWriterThread() {
+        super("ThreadGetSend");
+
+        try {
+            //connect the socket to localhost server
+            this.socket = new Socket("127.0.0.1", 2048);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void run() {
+        super.run();
+
+        try {
+            //get the user input to reader
+            out = new PrintWriter(socket.getOutputStream(), true);
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            String userInput;
+
+            while ((userInput = stdIn.readLine()) != null) {
+                //send the user input to socket
+                out.println(userInput);
+
+                if (userInput.equals("Bye")) {
+                    socket.close();
+                    break;
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
---
+++ SD_FP_04/SD_FP_04_ex2/src/WorkerThread.java
@@ -0,0 +1,45 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.net.*;
+import java.io.*;
+
+public class WorkerThread extends Thread {
+    private Socket socket = null;
+    BufferedReader in = null;
+
+    public WorkerThread(Socket socket) {
+        super("WorkerThread");
+        this.socket = socket;
+    }
+
+    public void run() {
+        try {
+            in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+
+            String message = null;
+
+            //while message from client is not null
+            while((message = in.readLine()) != null){
+                System.out.println("Mensagem do Cliente: " + message);
+
+                //add datetime to the message
+                LocalDateTime now = LocalDateTime.now();
+                String text = "" + dtf.format(now) + " " + socket.getLocalAddress().toString() + ":" + message;
+
+                //send message for all clients
+                Server.sendMessage(text);
+
+                //if the client sends bye, the thread for this client closes
+                if (message.equals("Bye")) {
+                    break;
+                }
+            }
+
+            in.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
